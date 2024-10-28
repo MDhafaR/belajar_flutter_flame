806e59eed168af0b3c05d78a2c81de13a0ef023a
Muhammad Dhafa Ramadhani <dhafaramadhani2004@gmail.com>
2024-10-28 14:28:09 +0700

    membuat dumble random berpindah tempat

--- lib/component/dumbbell_component.dart
+++ lib/component/dumbbell_component.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:belajar_flutter_flame/component/player_skinny_component.dart';
 import 'package:belajar_flutter_flame/constant/global.dart';
@@ -11,11 +12,20 @@ class DumbbellComponent extends SpriteComponent
     with HasGameRef<FitFighter>, CollisionCallbacks {
   final double _sizeDumbbell = 50;
 
+  // membuat dumbbell muncul secara random
+  Random _random = Random();
+
+  Vector2 _randomPosition() {
+    double x = _random.nextDouble() * gameRef.size.x;
+    double y = _random.nextDouble() * gameRef.size.y;
+    return Vector2(x, y);
+  }
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
     sprite = await gameRef.loadSprite(Globals.dumbbell);
-    position = Vector2(200, 200);
+    position = _randomPosition();
     height = width = _sizeDumbbell;
     anchor = Anchor.center;
 
@@ -29,6 +39,8 @@ class DumbbellComponent extends SpriteComponent
     if (other is PlayerSkinnyComponent) {
       FlameAudio.play(Globals.dumbbellSound);
       removeFromParent();
+      // membuat dumble muncul lagi ketika sudah hilang
+      gameRef.add(DumbbellComponent());
     }
   }
 }
