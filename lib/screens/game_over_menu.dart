1a25393d875341bb3a65e7b9d6c361b8279bf4bd
Muhammad Dhafa Ramadhani <dhafaramadhani2004@gmail.com>
2024-10-28 16:55:48 +0700

    menambahkan screen main menu dengan game over menu

---
+++ lib/screens/game_over_menu.dart
@@ -0,0 +1,68 @@
+import 'package:belajar_flutter_flame/constant/global.dart';
+import 'package:belajar_flutter_flame/flutter_game/fit_fighter.dart';
+import 'package:flutter/material.dart';
+
+class GameOverMenu extends StatelessWidget {
+  static const String ID = "GameOverMenu";
+
+  final FitFighter gameRef;
+  const GameOverMenu({super.key, required this.gameRef});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("assets/images/${Globals.background}"),
+          fit: BoxFit.cover,
+        )),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Game Over",
+                style: TextStyle(fontSize: 50),
+              ),
+              Text(
+                "Score: ${gameRef.dumbbellScore}",
+                style: const TextStyle(fontSize: 50),
+              ),
+              const SizedBox(
+                height: 200,
+              ),
+              SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                  },
+                  child: const Text(
+                    "Play Again ?",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                  },
+                  child: const Text(
+                    "Main Menu",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/main_menu.dart
@@ -0,0 +1,46 @@
+import 'package:belajar_flutter_flame/constant/global.dart';
+import 'package:flutter/material.dart';
+
+class MainMenu extends StatelessWidget {
+  const MainMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/${Globals.background}"),
+              fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 50),
+                child: Text(
+                  "Fit Fighter",
+                  style: TextStyle(fontSize: 50),
+                ),
+              ),
+              const SizedBox(
+                height: 200,
+              ),
+              SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Play",
+                      style: TextStyle(fontSize: 25),
+                    )),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
