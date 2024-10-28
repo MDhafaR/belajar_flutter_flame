df201d667f4a3badd83a981f80c2305af803ca33
Muhammad Dhafa Ramadhani <dhafaramadhani2004@gmail.com>
2024-10-28 16:25:00 +0700

    add score text

--- lib/flutter_game/fit_fighter.dart
+++ lib/flutter_game/fit_fighter.dart
@@ -9,9 +9,16 @@ import 'package:belajar_flutter_flame/constant/global.dart';
 import 'package:belajar_flutter_flame/inputs/joystick.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/text.dart';
 import 'package:flame_audio/flame_audio.dart';
+import 'package:flutter/material.dart';
 
 class FitFighter extends FlameGame with HasCollisionDetection {
+  int score = 0;
+  late Timer timer;
+  late TextComponent scoreText;
+  late int remainingTime;
+
   @override
   Future<void> onLoad() async {
     super.onLoad();
@@ -33,5 +40,28 @@ class FitFighter extends FlameGame with HasCollisionDetection {
       Globals.vaccineSound,
       Globals.dumbbellSound
     ]);
+
+    timer = Timer(
+      1,
+      repeat: true,
+      onTick: () {
+        if (remainingTime == 0) {
+          pauseEngine();
+        } else {
+          remainingTime--;
+        }
+      },
+    );
+
+    timer.start();
+
+    scoreText = TextComponent(
+        text: "Score: $score",
+        position: Vector2(50, 50),
+        anchor: Anchor.center,
+        textRenderer:
+            TextPaint(style: TextStyle(color: Colors.black, fontSize: 24)));
+
+    add(scoreText);
   }
 }
